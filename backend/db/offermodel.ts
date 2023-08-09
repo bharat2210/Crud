d4796a956e1d4f94503cb5ac2d7b4ebeac537f44
Bharat <bk4630132@gmail.com>
2023-08-09 15:49:46 +0530

    Offers add dynamically and Video added

--- backend/db/offermodel.ts
+++ backend/db/offermodel.ts
@@ -1,7 +1,6 @@
 const offer_mongoose = require("mongoose");
 const offerSchema = new offer_mongoose.Schema({
   imgPath: String,
-  
 });
 
 module.exports = offer_mongoose.model("offers", offerSchema);
